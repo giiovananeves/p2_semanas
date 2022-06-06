3df7ad52eb6593fa07b8497335193512d38a2c7c
giiovananeves <giovana.silva23@fatec.sp.gov.br>
2022-06-06 17:04:59 +0000

    concluida

---
+++ semanas.java
@@ -0,0 +1,12 @@
+public class semanas {
+
+    public static void main(String[] args) {
+        int segundaSemana = 5;
+        int terceiraSemana = 2;
+        int quartaSemana = 7;
+
+        System.out.println("Segunda Semana: "+segundaSemana);
+        System.out.println("Terceira Semana: "+terceiraSemana);
+        System.out.println("Quarta Semana: "+quartaSemana);
+    }
+}
